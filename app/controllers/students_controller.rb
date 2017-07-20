8aa7419864750fc8e3487af4a485ddd69071b2de
isharma-sharma <isharma.sharma55@gmail.com>
2017-07-19 21:30:01 -0400

    Changed  the student controller def set_student     @student = current_user.students.find(params[:id])   end   by this the user should have authentication to do action of student table

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -41,9 +41,8 @@ class StudentsController < OpenReadController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_student
-      @student = Student.find(params[:id])
+      @student = current_user.students.find(params[:id])
     end
-
     # Only allow a trusted parameter "white list" through.
     def student_params
       params.require(:student).permit(:name, :email, :address, :gender, :division, :dob)
--- scripts/update-students.sh
+++ scripts/update-students.sh
@@ -3,11 +3,11 @@ URL_PATH="/students"
 curl "${API}${URL_PATH}/1" \
   --include \
   --request PATCH \
-  --header "Authorization: Token token=BAhJIiUwODRiMzAyMjgzMDhjNzNkNDI2ZDVlZDFlOTk3NWNmMAY6BkVG--27d54a21ada6708e7e32f3b5fa83d2b59726418e" \
+  --header "Authorization: Token token=BAhJIiU2MDBlZGYxMzg2ZTU2ZWZiMTNkMmM5YjQ3YWUwMDQ0YQY6BkVG--5ab9ceb17f300a58ccbf8fbb45ff6144f2676e12" \
   --header "Content-Type: application/json" \
   --data '{
     "student": {
-      "division": "'"${DIVISION}"'",
-      "name": "'"${NAME}"'"
+      "division": "'"${1}"'",
+      "name": "'"${pooja}"'"
     }
   }'
